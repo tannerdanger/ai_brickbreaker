f9f6a3c31f688ff56a1457d19d35ac414111f169
tannerdanger <tannersbrown@gmail.com>
2019-02-18 01:16:26 +0000

    Added more AI logic

--- ai.js
+++ ai.js
@@ -1,17 +1,18 @@
 class AI {
     constructor() {
         console.log('hi from AI!')
-        
+
         this.weights = {
             LEFT: 0,
             RIGHT: 0,
         }
         this.isWatching = true
+        this.skip = false;
         this.reset();
-        
+        this.loop = 0;
     }
-   // watch(){this.isWatching = true; }
-    reset(){
+    // watch(){this.isWatching = true; }
+    reset() {
         this.weights.LEFT = 0;
         this.weights.RIGHT = 0;
     }
@@ -19,35 +20,60 @@ class AI {
 
         if (this.isWatching) {
             this.reset();
-            
+
             if (state._NAME === 'title') {
-                
+                this.skip = true;
             }
-            if (state._NAME === 'serve') {
-               
+            else if (state._NAME === 'serve') {
+                this.skip = true;
             }
             else if (state._NAME === 'play') {
                 this.calculatePlay();
             }
-            if (this.weights.LEFT < this.weights.RIGHT) {
-                console.log('holding ArrowLeft')
-                gDOWN_KEYS['ArrowLeft'] = true
-            } else {
-                console.log('holding ArrowRight')
-                gDOWN_KEYS['ArrowArrowRightLeft'] = true
+
+            if (!this.skip) {
+
+                if (this.weights.LEFT < this.weights.RIGHT) {
+                    //console.log('holding ArrowLeft')
+                    //gDOWN_KEYS['ArrowLeft'] = true
+                } else {
+                    console.log('holding ArrowRight')
+                    //gDOWN_KEYS['ArrowRight'] = true
+                }
+
             }
         }
 
     }
 
+    cleanup() {
+        this.reset();
+        //gDOWN_KEYS['ArrowRight'] = false
+        //gDOWN_KEYS['ArrowLeft'] = false
+        //gDOWN_KEYS['ArrowUP'] = false
+        //gDOWN_KEYS['ArrowDown'] = false
+        this.skip = false;
+
+    }
+
     calculatePlay() {
-        console.log('calculating play')
-        if (gBREAKER.x > gPLAYER.x) { this.weights.LEFT++ } else { this.weights.RIGHT++ }//if breaker is to left of paddle
-        //if(gBREAKER.dx < )
+        //console.log('calculating play')
+        this.loop++
+        //  if (this.loop > 100) {
+
+        console.log(this.loop)
+        //console.log('BREAKER MOVING UP:', this.isBreakerMovingUp());
+        //console.log('BREAKER BELOW BRICK LINE', this.isBreakerBelowBrickLine())
+ 
+            console.log('BREAKER IS ABOVE OF ME:')
+
+        console.log('--------------------')
+        //  }
+
     }
     pressKey(key) {
         this.ctx.dispatchEvent(new KeyboardEvent('keydown', { 'key': key }));
-        this.cleanup = function(){
+        this.cleanup = function () {
             this.ctx.dispatchEvent(new KeyboardEvent('keyup', { 'key': key }));
         }
     }
@@ -57,7 +83,48 @@ class AI {
     releaseKey(key) {
         this.ctx.dispatchEvent(new KeyboardEvent('keyup', { 'key': key }))
     }
+
+
+    //works
+    isBreakerMovingUp() {
+        return gBREAKER.dy < 0
+    }
+
+    isBreakerBelowBrickLine() {
+        return gBREAKER.y > BRICK_LINE
+    }
+
+    isBreakerAboveMe() {
+        if (gBREAKER.x < gPLAYER.x - (gPLAYER.w/2)) { //breaker is to left of player's mid - 1/2 its with - 1/2 breaker w
+            return false
+        }
+        if (gBREAKER.x > gPLAYER.x + (1.5* gPLAYER.w ) ) { //breaker is to left of player's mid - 1/2 its with - 1/2 breaker w
+            return false
+        }
+        return true
+    }
+
+    isBreakerCentered(){
+        if(gBREAKER.x < gPLAYER.x - gBREAKER.w){
+            return false
+        }
+        if(gBREAKER.x > gPLAYER.x + gBREAKER.w){
+            return false
+        }
+        return true
+    }
+
+    isBreakerLeftOfCenter(){
+        return gBREAKER.x < gPLAYER.x
+    }
     
+    isBreakerRightOfCenter(){
+        return gBREAKER.x > gPLAYER.x
+    }
+
+
+
+
 }
 
 //var CPU_GUY = new AI();
